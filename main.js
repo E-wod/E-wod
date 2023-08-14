3917667d449d811eb1b96ec850b89089d666ef35
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-08-13 22:26:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".navMenu");
-const navLink = document.querySelectorAll(".navLink");
-
-
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-    navLink.classList.toggle("active");
+    navMenu.classList.toggle("active");
 }
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburger.classList.remove("active");
-    navLink.classList.remove("active");
+    navMenu.classList.remove("active");
 }
