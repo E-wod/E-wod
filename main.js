7c197ce01183dda42e0262eb46e6a67b08583e59
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-08-17 00:10:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -27,16 +27,16 @@ function closeMenu() {
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.003}% ${50 - (_mouseY - _h) * 0.01}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.006}% ${50 - (_mouseY - _h) * 0.02}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.009}% ${50 - (_mouseY - _h) * 0.03}%`;
-	let _depth4 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.005}%`;
-	let _depth5 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.007}%`;
-	let _depth6 = `${50 - (_mouseX - _w) * 0.010}% ${50 - (_mouseY - _h) * 0.009}%`;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.003}% ${50 - (_mouseY - _h) * 0.001}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.006}% ${50 - (_mouseY - _h) * 0.002}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.009}% ${50 - (_mouseY - _h) * 0.003}%`;
+	let _depth4 = `${50 - (_mouseX - _w) * 0.012}% ${50 - (_mouseY - _h) * 0.005}%`;
+	let _depth5 = `${50 - (_mouseX - _w) * 0.018}% ${50 - (_mouseY - _h) * 0.007}%`;
+	let _depth6 = `${50 - (_mouseX - _w) * 0.021}% ${50 - (_mouseY - _h) * 0.009}%`;
 	let _depth7 = `${50 - (_mouseX - _w) * 0.022}% ${50 - (_mouseY - _h) * 0.013}%`;
-	let _depth8 = `${50 - (_mouseX - _w) * 0.022}% ${50 - (_mouseY - _h) * 0.013}%`;
-	let _depth9 = `${50 - (_mouseX - _w) * 0.016}% ${50 - (_mouseY - _h) * 0.016}%`;
-	let _depth10 = `${50 - (_mouseX - _w) * 0.022}% ${50 - (_mouseY - _h) * 0.019}%`;
+	let _depth8 = `${50 - (_mouseX - _w) * 0.022}% ${50 - (_mouseY - _h) * 0.015}%`;
+	let _depth9 = `${50 - (_mouseX - _w) * 0.024}% ${50 - (_mouseY - _h) * 0.017}%`;
+	let _depth10 = `${50 - (_mouseX - _w) * 0.026}% ${50 - (_mouseY - _h) * 0.019}%`;
 	let _depth11 = `${50 - (_mouseX - _w) * 0.029}% ${50 - (_mouseY - _h) * 0.022}%`;
 	let _depth13 = `${50 - (_mouseX - _w) * 0.036}% ${50 - (_mouseY - _h) * 0.025}%`;
 		
