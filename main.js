771c5c8da142c5b5c7b2a892e64c7fba46283874
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-09-09 12:44:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,29 @@ function closeMenu() {
 
 
 
-var figure = $(".video").hover( hoverVideo, hideVideo );
+const video = document.querySelector("video");
+let playState = null;
 
-function hoverVideo(e) { $('video', this).get(0).play(); }
-function hideVideo(e) { $('video', this).get(0).pause(); }
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      video.pause();
+      playState = false;
+    } else {
+      video.play();
+      playState = true;
+    }
+  });
+}, {});
+
+observer.observe(video);
+
+const onVisibilityChange = () => {
+  if (document.hidden || !playState) {
+    video.pause();
+  } else {
+    video.play();
+  }
+};
+
+document.addEventListener("visibilitychange", onVisibilityChange);
