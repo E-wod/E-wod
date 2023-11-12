bd5145957bc19024af1545ee4e1aa07420dc6e00
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-11-12 00:54:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,25 +16,7 @@ function closeMenu() {
 // GOTO TOP BUTTON
 
 
-// Get the button
-let jumpbutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    jumpbutton.style.display = "block";
-  } else {
-    jumpbutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 
 // Parallax 
