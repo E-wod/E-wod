5b7cedb19de715ac851cb5ff532f692b4f1faeed
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-11-12 00:18:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,16 +17,16 @@ function closeMenu() {
 
 
 // Get the button
-let mybutton = document.getElementById("myBtn");
+let jumpbutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    jumpbutton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    jumpbutton.style.display = "none";
   }
 }
 
