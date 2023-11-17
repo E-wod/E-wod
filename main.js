89c1e4dc95673c3af667ec17fec092e2c741663f
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-11-16 21:35:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,20 +15,8 @@ function closeMenu() {
 
 // GOTO TOP BUTTON
 
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 var rootElement = document.documentElement;
-
-function handleScroll() {
-  // Do something on scroll
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if (rootElement.scrollTop / scrollTotal > 0.8) {
-    // Show button
-    scrollToTopBtn.classList.add("showBtn");
-  } else {
-    // Hide button
-    scrollToTopBtn.classList.remove("showBtn");
-  }
-}
 
 function scrollToTop() {
   // Scroll to top logic
@@ -38,7 +26,6 @@ function scrollToTop() {
   });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
-document.addEventListener("scroll", handleScroll);
 
 
 
