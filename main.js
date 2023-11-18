f9db7d663c6464f23e01ef18a535ef8733efdd64
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-11-17 19:31:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -28,31 +28,4 @@ function scrollToTop() {
 scrollToTopBtn.addEventListener("click", scrollToTop);
 
 
-
-	
-// Parallax 
-(function() {
-    document.addEventListener("mousemove", parallax);
-    const elem = document.querySelector("#parallax");
-    function parallax(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.003}% ${50 - (_mouseY - _h) * 0.001}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.006}% ${50 - (_mouseY - _h) * 0.002}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.009}% ${50 - (_mouseY - _h) * 0.003}%`;
-	let _depth4 = `${50 - (_mouseX - _w) * 0.013}% ${50 - (_mouseY - _h) * 0.005}%`;
-	let _depth5 = `${50 - (_mouseX - _w) * 0.030}% ${50 - (_mouseY - _h) * 0.020}%`;
-	let _depth6 = `${50 - (_mouseX - _w) * 0.018}% ${50 - (_mouseY - _h) * 0.009}%`;
-	let _depth7 = `${50 - (_mouseX - _w) * 0.019}% ${50 - (_mouseY - _h) * 0.013}%`;
-	let _depth8 = `${50 - (_mouseX - _w) * 0.020}% ${50 - (_mouseY - _h) * 0.015}%`;
-	let _depth9 = `${50 - (_mouseX - _w) * 0.028}% ${50 - (_mouseY - _h) * 0.027}%`;
-	let _depth10 = `${50 - (_mouseX - _w) * 0.024}% ${50 - (_mouseY - _h) * 0.019}%`;
-	let _depth11 = `${50 - (_mouseX - _w) * 0.025}% ${50 - (_mouseY - _h) * 0.020}%`;
-	let _depth13 = `${50 - (_mouseX - _w) * 0.036}% ${50 - (_mouseY - _h) * 0.025}%`;
-        let x = `${_depth13}, ${_depth11}, ${_depth10}, ${_depth9}, ${_depth8}, ${_depth7}, ${_depth6}, ${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
-        console.log(x);
-        elem.style.backgroundPosition = x;
-    }
 })();
