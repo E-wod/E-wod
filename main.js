64e3079c87edf023f02974dd65b1bc3cd2837dbf
Eric Souza <47480812+E-wod@users.noreply.github.com>
2023-11-20 17:25:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,16 +15,18 @@ function closeMenu() {
 
 // GOTO TOP BUTTON
 
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-var rootElement = document.documentElement;
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
 
 function scrollToTop() {
   // Scroll to top logic
   rootElement.scrollTo({
     top: 0,
-    behavior: "smooth";)
+    behavior: "smooth"
+  })
 }
-scrollToTopBtn.addEventListener("click", scrollToTop);
+
+scrollToTopBtn.addEventListener("click", scrollToTop)
 
 
 })();
