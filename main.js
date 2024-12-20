77dd41374fda0a88327d141c8b561fcef13bb977
Eric Souza <47480812+E-wod@users.noreply.github.com>
2024-12-20 11:59:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const splash = document.querySelector('.splash');
+    setTimeout(() => {
+        splash.classList.add('fade-out');
+        setTimeout(() => {
+            splash.style.display = 'none';
+            document.querySelector('.content').style.display = 'block';
+        }, 1000);
+    }, 2000);
+});
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
