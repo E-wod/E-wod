b833a7be45933d3b6c914bb2b4be259a801743d9
Eric Souza <47480812+E-wod@users.noreply.github.com>
2024-12-24 14:14:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -28,19 +28,3 @@ function scrollToTop() {
   });
 }
 scrollToTopBtn.addEventListener("click", scrollToTop);
-
-
-
-let cardNumInput = 
-    document.querySelector('#cardNum')
-
-cardNumInput.addEventListener('keyup', () => {
-    let cNumber = cardNumInput.value
-    cNumber = cNumber.replace(/\s/g, "")
-
-    if (Number(cNumber)) {
-        cNumber = cNumber.match(/.{1,4}/g)
-        cNumber = cNumber.join(" ")
-        cardNumInput.value = cNumber
-    }
-})
