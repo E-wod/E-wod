2a94626a8475bf040dda1a43bcf04b3dcbff9ace
Eric Souza <47480812+E-wod@users.noreply.github.com>
2025-04-14 11:40:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,11 @@
-// Hambuurger
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-hamburger.classList.toggle("active");
-navMenu.classList.toggle("active");
-}
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-hamburger.classList.remove("active");
-navMenu.classList.remove("active");
-}
 // GOTO TOP BUTTON
 var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 var rootElement = document.documentElement;
 scrollToTopBtn.addEventListener("click", scrollToTop);
 function scrollToTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
+
+rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
-  });
-// Splash
-import React from 'react';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-
-const App = () => {
-  return (
-    <DotLottieReact
-      src="https://lottie.host/87523996-ed8c-4e4e-9199-146bf0ae91fe/xUvamU8EDS.lottie"
-      loop
-      autoplay
-    />
-  );
-};
-import { DotLottie } from '@lottiefiles/dotlottie-web';
-
-const dotLottie = new DotLottie({
-    autoplay: true,
-    loop: true,
-    canvas: document.querySelector('#dotlottie-canvas'),
-    src: "<https://lottie.host/YOUR_ANIMATION_ID.lottie>", // replace with your .lottie or .json file URL
 });
-  
+// Splash
