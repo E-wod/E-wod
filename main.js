2d7fe044fd05a61469538e38b56185d8b8c32a73
Eric Souza <47480812+E-wod@users.noreply.github.com>
2025-04-15 12:43:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-// GOTO TOP BUTTON
-var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-var rootElement = document.documentElement;
-scrollToTopBtn.addEventListener("click", scrollToTop);
-function scrollToTop() {
 
-rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-});
-// Splash
