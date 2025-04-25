8545041d0ec49412c798ed588f4df4c5b1172614
Eric Souza <47480812+E-wod@users.noreply.github.com>
2025-04-25 14:02:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,148 @@
 let images = document.querySelectorAll("landingContent");
 lazyload(images);
 
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+public class ImageUtil 
+{
 
-public class ImageResizer {
-    public static void main(String[] args) {
-        try {
-            File inputFile = new File("img/");
-            BufferedImage inputImage = ImageIO.read(inputFile);
+    private static RenderingHints hints;
 
-            int scaledWidth = 800; // desired width
-            int scaledHeight = 600; // desired height
+    static 
+    {
+        hints = new RenderingHints(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        hints.put(RenderingHints.KEY_COLOR_RENDERING,RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
-            BufferedImage outputImage = new BufferedImage(scaledWidth, scaledHeight, inputImage.getType());
-            Graphics2D g2d = outputImage.createGraphics();
-            g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
-            g2d.dispose();
+        /*
+         * this block is to silence the warning that we're not using JAI
+         * native acceleration but are using the pure java implementation.
+         */
+        Properties p = new Properties(System.getProperties());
+        p.put("com.sun.media.jai.disableMediaLib", "true");
+        System.setProperties(p);
+    }  
 
-            File outputFile = new File("img/");
-            ImageIO.write(outputImage, "jpg", outputFile);
 
-            System.out.println("Image resized successfully!");
-        } catch (IOException e) {
-            System.err.println("Error resizing the image: " + e.getMessage());
+    public static byte[] getScaledInstance(byte[] image, int maxWidth) 
+    {
+        InputStream in = new ByteArrayInputStream(image);
+        BufferedImage img = null;
+        try 
+        {
+            img = ImageIO.read(in);
+
+        double scale = (double) maxWidth / img.getWidth();
+        if ( scale > 1.0d ) 
+        {
+                //return getByteArray(getScaledUpGraphics(img, scale));
+            return image;
+        } 
+        else if (scale > 0.5d && scale < 1.0d) 
+        {
+                return getByteArray(getScaledDownByGraphics(img, scale));
+        } 
+        else if (scale <= 0.5d) 
+        {
+                return getByteArray(getScaledDownByJAI(img, scale));
         }
+        // else scale == 1.0d; no change required.
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    public static byte[] getByteArray(BufferedImage img)
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] imageInByte = null;
+        try 
+        {
+            ImageIO.write( img, "jpg", baos );
+            baos.flush();
+            imageInByte = baos.toByteArray();
+            baos.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+
+        return imageInByte;
+    }
+
+    /**
+     * See http://www.digitalsanctuary.com/tech-blog/java/how-to-resize-uploaded-images-using-java-better-way.html
+     * This instance seems to produce quality images ONLY when you are
+     * scaling down to something less than 50% of the original size.
+     * @param img
+     * @param scale
+     * @return the scaled image
+     */
+    private static BufferedImage getScaledDownByJAI(BufferedImage img, double scale) 
+    {
+        if(scale > 1.0d) {
+                throw new RuntimeException("Can't scale according to " + scale + " : This method only scales down.");
+        }
+        PlanarImage originalImage = PlanarImage.wrapRenderedImage(img);
+        // now resize the image
+        ParameterBlock paramBlock = new ParameterBlock();
+        paramBlock.addSource(originalImage); // The source image
+        paramBlock.add(scale); // The xScale
+        paramBlock.add(scale); // The yScale
+        paramBlock.add(0.0); // The x translation
+        paramBlock.add(0.0); // The y translation
+        RenderedOp resizedImage = JAI.create("SubsampleAverage", paramBlock, hints);
+        return resizedImage.getAsBufferedImage();      
+    }
+
+    /**
+     * This method produces high quality images when target scale is greater
+     * than 50% of the original.
+     * @param img
+     * @param scale
+     * @return the scaled image
+     */
+    private static BufferedImage getScaledDownByGraphics(BufferedImage img, double scale) 
+    {
+        final float scaleFactor = 0.8f;
+        BufferedImage ret = (BufferedImage)img;
+        int w = img.getWidth();
+        int h = img.getHeight();
+
+        int targetWidth = (int)(img.getWidth() * scale);
+        int targetHeight = (int)(img.getHeight() * scale);
+
+        int loopCount = 0;
+        int maxLoopCount = 20;
+        BufferedImage tmp;
+        do 
+        {
+            if (w > targetWidth) {
+                    w *= scaleFactor;
+                    if (w < targetWidth) {
+                            w = targetWidth;
+                    }
+            }
+            if (h > targetHeight) {
+                    h *= scaleFactor;
+                    if (h < targetHeight) {
+                            h = targetHeight;
+                    }
+            }
+            tmp = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2 = tmp.createGraphics();
+
+            g2.addRenderingHints(hints);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
+
+            ret = tmp;
+            if(++loopCount > maxLoopCount) {
+                    throw new RuntimeException("Hit maximum loop count " + maxLoopCount);
+            }
+        } while (w != targetWidth || h != targetHeight);
+        return ret;    
     }
 }
