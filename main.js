a91640ecb0e5c03a4ed973f0771d6284526ca98b
Eric Souza <47480812+E-wod@users.noreply.github.com>
2025-05-22 09:19:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const image = document.getElementById('img/ESDLogo.png');
+    image.addEventListener('load', () => {
+      image.style.display = 'block';
+      
+      
 preload_image("https://imagizer.imageshack.com/img924/3995/kPr8m0.jpg"); 
 function preload_image(im_url) {
   let img = new Image();
