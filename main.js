26b00b25b713ad84da1679e26784bcd761c46b9b
Eric Souza <47480812+E-wod@users.noreply.github.com>
2026-05-05 11:45:57 -0400

    Update main.js

--- main.js
+++ main.js
@@ -364,7 +364,7 @@ function animateArticleFixedLayer(gsap, article, index) {
         immediateRender: false,
         scrollTrigger: {
           trigger: article,
-          start: "top 115%",
+          start: "top 145%",
           end: "top top",
           scrub: 0.5
         }
